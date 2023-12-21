1a3fbfa09435920c2ec6269d3dd95d93be71ffe6
raivolink <75722172+raivolink@users.noreply.github.com>
2023-12-21 20:47:53 +0200

    supply_chain_challenge

---
+++ example-supply-chain-challenge/README.md
@@ -0,0 +1,37 @@
+# A robot that completes the supply chain challenge
+
+<img src="images/purchase-orders-web-app.png" style="margin-bottom:20px" alt="po_app">
+
+This example robot completes the supply chain challenge at <https://developer.automationanywhere.com/challenges/automationanywherelabs-supplychainmanagement.html>.
+
+The robot uses the [robocorp-playwright](https://robocorp.com/docs/python/robocorp/robocorp-browser) library for browser automation, [RPA.HTTP](https://robocorp.com/docs/libraries/rpa-framework/rpa-http) library for downloading the CSV file used in the challenge and the [Polars](https://pola-rs.github.io/polars/py-polars/html/index.html) library for reading the excel into a data structure.
+
+```python
+@task
+def solve_challenge():
+    """Solve the RPA challenge"""
+    browser.configure(
+        browser_engine="chromium",
+        screenshot="only-on-failure",
+        headless=True,
+    )
+    # user_agent is neede for headless runs
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    context = browser.context()
+    context.set_extra_http_headers({"User-Agent": user_agent})
+    # navigate to challenge page
+    browser.goto(URL)
+    # get customer data as dataframe
+    df_customers = get_customers_data()
+    # fill data to crm
+    for customer in df_customers.rows(named=True):
+        fill_and_submit_form(customer)
+    # log screenshot and challlenge id
+    challenge_verification()
+```
+
+For more information, do not forget to checkout the following:
+
+* [Robocorp Documentation -site](https://robocorp.com/docs/python/robocorp)
+* [Portal for more examples](https://robocorp.com/portal)
+* [robo repo](https://github.com/robocorp/robo) as this will developed a lot...
---
+++ example-supply-chain-challenge/tasks.py
@@ -0,0 +1,114 @@
+from robocorp import browser, log
+from pathlib import Path
+import polars as pl
+from robocorp.tasks import get_output_dir, task
+
+from RPA.HTTP import HTTP
+
+OUTPUT_DIR = get_output_dir() or Path("output")
+EXCEL_PATH = OUTPUT_DIR / "agents.xlsx"
+PROCUREMENT_URL = "https://developer.automationanywhere.com/challenges/AutomationAnywhereLabs-POTrackingLogin.html"
+PURCHASE_ORDERS_URL = "https://developer.automationanywhere.com/challenges/automationanywherelabs-supplychainmanagement.html"
+
+
+@task
+def solve_challenge():
+    """Complete the supply chain challenge"""
+    launch_browser()
+    open_procurement_website_and_log_in()
+    purchase_order_page = open_po_page()
+    df_agents = get_agents(purchase_order_page)
+    po_numbers = get_po_numbers(purchase_order_page)
+    get_po_data_and_insert_to_form(df_agents, po_numbers, purchase_order_page)
+    challenge_verification(purchase_order_page)
+
+
+def launch_browser():
+    browser.configure(
+        browser_engine="chromium",
+        screenshot="only-on-failure",
+        headless=False,
+    )
+    # user_agent is neede for headless runs
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    context = browser.context()
+    context.set_extra_http_headers({"User-Agent": user_agent})
+
+
+def open_po_page():
+    context = browser.context()
+    purchase_order_page = context.new_page()
+    purchase_order_page.goto(PURCHASE_ORDERS_URL)
+    return purchase_order_page
+
+
+def get_agents(purchase_order_page):
+    download_xlsx_element = purchase_order_page.query_selector(".challenge-intro a.btn")
+    csv_url = download_xlsx_element.get_attribute("href")
+    HTTP().download(csv_url, EXCEL_PATH, overwrite=True)
+    df_agents = pl.read_excel(EXCEL_PATH)
+    return df_agents
+
+
+def get_po_numbers(purchase_order_page):
+    po_numbers = {}
+    po_list = [f"PONumber{number}" for number in range(1, 8)]
+    for po in po_list:
+        po_number = purchase_order_page.locator(f"#{po}").input_value()
+        po_numbers[po] = po_number
+    return po_numbers
+
+
+def search_for_po(po_number, page):
+    page.fill("#dtBasicExample_filter input", po_number)
+
+
+def get_po_data_and_insert_to_form(df_agents, po_numbers, purchase_order_page):
+    page = browser.page()
+    page.bring_to_front()
+    for po in po_numbers:
+        # TODO Clearer logic(using enumerate?)
+        search_for_po(po_numbers[po], page)
+        state = page.locator("#dtBasicExample td:nth-child(5)").inner_text()
+        ship_date = page.locator("#dtBasicExample td:nth-child(7)").inner_text()
+        order_total = page.locator("#dtBasicExample td:nth-child(8)").inner_text()[1:]
+        agent_in_state = df_agents.filter(df_agents["State"] == state)
+        print(state, ship_date, order_total, agent_in_state["Full Name"][0])
+        # TODO Change logic for not switch pages
+        fill_po_form(
+            po[-1],
+            ship_date,
+            order_total,
+            agent_in_state["Full Name"][0],
+            purchase_order_page,
+        )
+
+    purchase_order_page.locator("#submit_button").click()
+
+
+def open_procurement_website_and_log_in():
+    browser.goto(PROCUREMENT_URL)
+    page = browser.page()
+    page.click("#onetrust-accept-btn-handler", no_wait_after=True)
+    page.fill("#inputEmail", "admin@procurementanywhere.com")
+    page.fill("#inputPassword", "paypacksh!p")
+    page.click(".btn-primary")
+
+
+def fill_po_form(index, ship_date, total, agent, purchase_order_page):
+    purchase_order_page.bring_to_front()
+    purchase_order_page.fill(f"#shipDate{index}", str(ship_date))
+    purchase_order_page.fill(f"#orderTotal{index}", str(total))
+    purchase_order_page.query_selector(f"#agent{index}").select_option(str(agent))
+
+
+def challenge_verification(purchase_order_page):
+    """
+    Function to add challenge result screenshot
+    and id to generated log
+    """
+    purchase_order_page.wait_for_selector("css=.modal-body")
+    completion_modal = purchase_order_page.locator("css=.modal-body")
+    browser.screenshot(completion_modal)
+    completion_id = purchase_order_page.locator("id=guidvalue").input_value()
+    log.info(f"Challenge completion id: {completion_id}")
